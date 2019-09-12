c7e06570e4ca729394577fb4b78999a826e02112
Hwee Lern <hweelern@sgag.sg>
2019-09-13 00:42:56 +0800

    Add test for input field

--- src/Components/__tests__/SearchBar.test.js
+++ src/Components/__tests__/SearchBar.test.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 import { SearchBar } from "../SearchBar";
+import {
+  Button,
+  TextField,
+} from "@material-ui/core";
+
+let wrapper;
 
 describe("Search Bar", () => {
+  beforeEach(() => {
+    wrapper = shallow(<SearchBar/>)
+  });
+
+  it("should have a input field", () => {
+    const textField = wrapper.find(TextField)
+    expect(textField.length).toBe(1);
+    expect(textField.props().id).toBe("username-textfield");
+  })
+
   it("should have a search button", () => {
-    let wrapper = shallow(
-      <SearchBar/>,
-    );
     expect(wrapper.find("#search-button").length).toBe(1);
     expect(wrapper.find("#search-button").text()).toBe("Search");
   })
