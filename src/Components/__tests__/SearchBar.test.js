4a8a00ead0eb8a3867445d92c15de0725274c6c9
Hwee Lern <hweelern@sgag.sg>
2019-09-13 06:42:56 +0800

    Create test for form submit simulation

--- src/Components/__tests__/SearchBar.test.js
+++ src/Components/__tests__/SearchBar.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import axios from "axios";
 import toJson from "enzyme-to-json";
 import { SearchBar } from "../SearchBar";
 import {
@@ -7,26 +8,44 @@ import {
   TextField,
 } from "@material-ui/core";
 
+jest.mock("axios");
 let wrapper;
 
-describe("Search Bar", () => {
+describe("SearchBar", () => {
   beforeEach(() => {
     wrapper = shallow(<SearchBar/>)
   });
 
-  it("should have a form", () => {
-    expect(wrapper.find("form").length).toBe(1);
+  describe("form", () => {
+    it("should render successfully", () => {
+      expect(wrapper.find("form").length).toBe(1);
+    });
   })
 
-  it("should have a input field", () => {
-    const textField = wrapper.find(TextField)
-    expect(textField.length).toBe(1);
-    expect(textField.props().id).toBe("username-textfield");
+  describe("input field", () => {
+    it("should render successfully", () => {
+      const textField = wrapper.find(TextField)
+      expect(textField.length).toBe(1);
+      expect(textField.props().id).toBe("username-textfield");
+    });
   })
-
-  it("should have a search button", () => {
-    const button = wrapper.find(Button)
-    expect(button.length).toBe(1);
-    expect(button.children().text()).toBe("Search")
+  
+  describe("search button", () => {
+    it("should render successfully", () => {
+      const button = wrapper.find(Button)
+      expect(button.length).toBe(1);
+      expect(button.children().text()).toBe("Search");
+      expect(button.props().disabled).toBe(true);
+    });
+    it("should call upon endpoint when click search btn", () => {
+      wrapper
+        .find(TextField)
+        .simulate("change", { target: { value: "username" }});
+      wrapper
+        .find("form")
+        .simulate("submit", { preventDefault: jest.fn() });
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(axios.get).toHaveBeenCalledWith("https://api.github.com/users/username/repos");
+    })
   })
 })
