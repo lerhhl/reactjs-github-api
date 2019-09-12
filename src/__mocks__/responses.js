a03afbd2c1fc6626945f95b54be2c5156c3ebf6e
Hwee Lern <hweelern@sgag.sg>
2019-09-13 06:39:08 +0800

    Create mock responses

---
+++ src/__mocks__/responses.js
@@ -0,0 +1,9 @@
+export const repo1 = {
+  name: "repo1",
+  html_url: "https://github.com/username/repo1"
+}
+
+export const repo2 = {
+  name: "repo2",
+  html_url: "https://github.com/username/repo2"
+}
