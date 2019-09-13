b1bb795ba3bd7b1b5c65c95be40ae7eb43587e29
Hwee Lern <hweelern@sgag.sg>
2019-09-13 23:00:42 +0800

    Update App.test.js for redux store

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import App from './App';
+import reducers from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
