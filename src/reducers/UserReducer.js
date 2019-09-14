017136015d4e72eabe01107607add55f57609f28
Hwee Lern <hweelern@sgag.sg>
2019-09-14 17:10:25 +0800

    Add UPDATE_USER_ORGS to UserReducer and create test

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,14 +1,24 @@
-import { UPDATE_USER_REPOS } from "../actions/types";
+import {
+  UPDATE_USER_REPOS,
+  UPDATE_USER_ORGS,
+} from "../actions/types";
 
 const INITIAL_STATE = {
   repos: [],
+  orgs: [],
 }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case UPDATE_USER_REPOS:
     return {
+      ...state,
       repos: action.payload,
+    };
+  case UPDATE_USER_ORGS:
+    return {
+      ...state,
+      orgs: action.payload,
     };
   default:
     return state;
--- src/reducers/__tests__/reducers.test.js
+++ src/reducers/__tests__/reducers.test.js
@@ -1,5 +1,11 @@
 import { UserReducer } from "../index";
 import * as types from "../../actions/types";
+import {
+  repo1,
+  repo2,
+  org1,
+  org2,
+} from "../../__mocks__/responses";
 
 describe("reducers", () => {
   describe("User Reducer", () => {
@@ -7,37 +13,35 @@ describe("reducers", () => {
       it("should return default state", () => {
         expect(UserReducer(undefined, {})).toStrictEqual({
           repos: [],
+          orgs: [],
         });
       });
     });
 
     describe("UPDATE_USER_REPOS", () => {
       it("should update the user state after getting user repos", () => {
-        const payload = [
-          {
-            name: "repo1",
-            html_url: "repo1_html_url"
-          },
-          {
-            name: "repo2",
-            html_url: "repo2_html_url"
-          },
-        ];
+        const payload = [repo1, repo2];
         const action = {
           type: types.UPDATE_USER_REPOS,
           payload,
         };
         expect(UserReducer(undefined, action)).toStrictEqual({
-          repos: [
-            {
-              name: "repo1",
-              html_url: "repo1_html_url",
-            },
-            {
-              name: "repo2",
-              html_url: "repo2_html_url",
-            },
-          ],
+          orgs: [],
+          repos: [repo1, repo2],
+        });
+      });
+    });
+
+    describe("UPDATE_USER_ORGS", () => {
+      it("should update the user state after getting user orgs", () => {
+        const payload = [org1, org2];
+        const action = {
+          type: types.UPDATE_USER_ORGS,
+          payload,
+        };
+        expect(UserReducer(undefined, action)).toStrictEqual({
+          orgs: [org1, org2],
+          repos: [],
         });
       });
     });
