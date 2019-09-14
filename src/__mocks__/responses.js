43d3688dadbe4d337d01c8db9aaab1f7e7f22923
Hwee Lern <hweelern@sgag.sg>
2019-09-14 16:55:38 +0800

    Add two organizations data

--- src/__mocks__/responses.js
+++ src/__mocks__/responses.js
@@ -7,3 +7,15 @@ export const repo2 = {
   name: "repo2",
   html_url: "https://github.com/username/repo2"
 }
+
+export const org1 = {
+  name: "org1",
+  description: "org1 description",
+  html_url: "github.com/org1",
+}
+
+export const org2 = {
+  name: "org2",
+  description: "org2 description",
+  html_url: "github.com/org2",
+}
